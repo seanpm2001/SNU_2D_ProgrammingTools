9eee70ab24b91d06862323a87325b71492779da2
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 14:51:07 -0700

    2 Test VHDL scripts
    2 test VHDL scripts, mainly syntax tests.

---
+++ Programming Tools/VirtualHardDiskLanguage/Test1.vhdl
@@ -0,0 +1,25 @@
+-- Start of file
+WRITE "CORRUPT" TO V://Root
+IN FILE: hard.sys
+X = 12
+I = 1
+FOR I in range(5)
+	WRITE "TEST" TO "TEST"
+WHILE X = X:
+	WRITE "1" TO V://Root
+begin buffer(6)
+block []
+block buffer
+end block
+end file
+write "a" out
+-- Just figuring out the syntax. Do not run this, as it won't work (100% guarantee) Virtual Hard Disk Language file.
+-- File info:
+-- Name: Test1
+-- Extension: .VHDL (Virtual Hard Disk Language)
+-- Language: VHDL (Virtual Hard Disk Language)
+-- Stable? No
+-- Functional? No
+-- Version 1: September 8th 2019
+-- Latest version: 1
+-- End of file
