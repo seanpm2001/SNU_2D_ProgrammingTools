9a4be16b4bfa238843592e3e3639f95184fcffcd
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 13:24:27 -0700

    4 Objective-C scripts
    4 scripts written in Objective-C.

---
+++ Programming Tools/Objective-C/Samples/InterfaceDeclareV1.mm
@@ -0,0 +1,13 @@
+@interface classname : superclassname {
+ // instance variables
+}
++ classMethod1;
++ (return_type)classMethod2;
++ (return_type)classMethod3:(param1_type)param1_varName;
+
+- (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+- (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName param2_callName:(param2_type)param2_varName;
+@end
+// From: Wikipedia https://en.wikipedia.org/wiki/Objective-C
+// File version: 1 (October 23rd 2019)
+// First version: 1 (October 23rd 2019)
---
+++ Programming Tools/Objective-C/Samples/object-method-argument.mm
@@ -0,0 +1,9 @@
+// In C++
+/*
+obj->method(argument);
+*/
+// In Objective-C
+[obj method:argument];
+// From: https://en.wikipedia.org/wiki/Objective-C
+// File version: 1 (October 23rd 2019)
+// First version: 1 (October 23rd 2019)
