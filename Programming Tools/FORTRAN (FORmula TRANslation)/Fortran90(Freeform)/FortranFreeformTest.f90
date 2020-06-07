f48c9af4fba384e57aa2dd573f0b82f69caa6f24
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 12:27:27 -0700

    2 FORTRAN 90 Freeform scripts
    2 FORTRAN 90 (Freeform) scripts

---
+++ Programming Tools/FORTRAN (FORmula TRANslation)/Fortran90(Freeform)/FortranFreeformTest.f90
@@ -0,0 +1,12 @@
+! Arabic numeral system
+01234 ! 0 to 4
+56789 ! 5 to 9
+! fortran freeform file
+program test1 ! Defines the program
+	print *, "Welcome to FORTRAN90" ! Prints output
+	print *, "This is a freeform FORTRAN test file" ! Prints output
+	print *, "Freeform is for FORTRAN versions 90 to modern" ! Prints output
+end program test1 ! Ends the program
+! End
+! File version: 1 (October 24th 2019)
+! First version: 1 (October 24th 2019)
