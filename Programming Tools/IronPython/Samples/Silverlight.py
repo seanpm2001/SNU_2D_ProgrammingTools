bb7a4473272437eef41b3d26e512516ad61c522d
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 13:04:07 -0700

    2 Iron Python files
    2 Iron Python files from Wikipedia.

---
+++ Programming Tools/IronPython/Samples/Silverlight.py
@@ -0,0 +1,7 @@
+//DLR initialization script.
+<script src="http://gestalt.ironpython.net/dlr-latest.js" type="text/javascript"></script>
+
+//Client-side script passed to IronPython and Silverlight.
+<script type="text/python">
+    window.Alert("Hello from Python")
+</script>
