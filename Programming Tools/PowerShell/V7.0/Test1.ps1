5282e27cb42e13cd27d1886092d08fed38e56405
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 13:46:42 -0700

    2 PowerShell scripts, 2 PowerShell compatibility notes
    2 PowerShell scripts, along with 2 PowerShell compatibility note files.

---
+++ Programming Tools/PowerShell/V7.0/Test1.ps1
@@ -0,0 +1,14 @@
+# Start
+<# Commented out for safety reasons
+h
+r
+write "hello world."
+del
+#>
+# End
+<# File info
+Powershell file .ps1
+Testing for syntax
+first version: 1 (September 8th 2019)
+Latest version: 1 (September 8th 2019)
+#>
