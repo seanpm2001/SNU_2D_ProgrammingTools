469b01caa6424daf1cc34151c47c913e526e7dcf
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 13:30:17 -0700

    4 PHP scripts
    4 PHP scripts.

---
+++ Programming Tools/PHP (PHP Hypertext Preprocessor)/Samples/PHPBasicSyntaxAndComments.php
@@ -0,0 +1,7 @@
+<? // A PHP script always starts with a < with a following ? and ends with a ? following a >
+// This is a single line comment. It starts with //
+/* This is a block comment.
+It startd with a / and a * and allows for multi-line comment
+it ends with a * and /
+*/
+?>
---
+++ Programming Tools/PHP (PHP Hypertext Preprocessor)/Samples/VarTest.php
@@ -0,0 +1,12 @@
+<? // Bad PHP example script
+$s = "$s";
+$money = "$DollarSign|Money|DollarSign"; { // Scopes don't work, just structure (makes script inoperable)
+	echo("Variable $'s don't mean $money");
+}
+$variable = "$Variable"; { // Scopes don't work, just structure (makes script inoperable)
+	echo("They are considered $variable s");
+}
+/* File version: 1 (October 8th 2019)
+First version: 1 (October 8th 2019)
+Latest version: 1 ((October 8th 2019)
+*/ ?>
