cfc65b34e084d518e36f0c4b9dd11ab0f833fe31
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-07 14:11:00 -0700

    SQL Injection [2x]
    2 files about SQL injection, this version based on the XKCD comic of it.

---
+++ Programming Tools/SQL (Structured Query Language)/Samples/SQL_InjectionXKCD.sql
@@ -0,0 +1,7 @@
+-- SQL Injection XKCD joke
+-- TABLE students;
+q = "INSERT INTO Students VALUES ('" + FNMName.Text + "', '" + LName.Text + "')";
+INSERT INTO Students VALUES ('Robert'); DROP TABLE Students; --', 'Derper')
+-- File version: 1 (October 15th 2019)
+-- Latest version: 1 (October 15th 2019)
+-- First version: 1 (October 15th 2019)
