2916f4033d5e8128c5133b1e4b429decb6e99767
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-06 23:33:50 -0700

    4 example C# programs
    4 C# Example programs.

---
+++ Programming Tools/C_Sharp/Examples/BoxingUnboxing.cs
@@ -0,0 +1,6 @@
+// From: https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+int foo = 42;         // Value type.
+object bar = foo;     // foo is boxed to bar.
+int foo2 = (int)bar;  // Unboxed back to value type.
+// File version: 1 (October 21st 2019)
+// Latest version: 1 (October 21st 2019)
---
+++ Programming Tools/C_Sharp/Examples/HelloWorldInCSharpV1.cs
@@ -0,0 +1,11 @@
+// From: https://en.wikipedia.org/wiki/C_Sharp_(programming_language)
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
+// File version: 1 (October 21st 2019)
+// Latest version: 1 (October 21st 2019)
