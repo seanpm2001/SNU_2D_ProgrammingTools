e576602b1e75a033a32afb0b8bf8ce9b603a360a
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-09-28 19:36:58 -0700

    WP_DeclarationFileV1.tsx
    An archive of the first version of the Wikipedia Declaration file TypeScript example.

---
+++ Programming Tools/TypeScript/Samples/OldVersions/WP_DeclarationFileV1.tsx
@@ -0,0 +1,6 @@
+declare namespace arithmetics {
+    add(left: number, right: number): number;
+    subtract(left: number, right: number): number;
+    multiply(left: number, right: number): number;
+    divide(left: number, right: number): number;
+}
