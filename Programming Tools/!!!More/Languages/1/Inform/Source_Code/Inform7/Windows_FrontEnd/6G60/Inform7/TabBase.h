577f1997fc327c91beb354c82808a5fd5053adfc
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-17 21:17:41 -0700

    31 files
    31 files for this directory, in C, and c++ formats.

---
+++ Programming Tools/!!!More/Languages/1/Inform/Source_Code/Inform7/Windows_FrontEnd/6G60/Inform7/TabBase.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "TabInterface.h"
+
+class TabBase : public CWnd, public TabInterface
+{
+  DECLARE_DYNAMIC(TabBase)
+
+public:
+  void MakeInactive(void);
+  bool IsEnabled(void);
+  COLORREF GetTabColour(void);
+
+  void OpenProject(const char* path, bool primary);
+  bool SaveProject(const char* path, bool primary);
+  void CompileProject(CompileStage stage, int code);
+  bool IsProjectEdited(void);
+  void Progress(const char* msg);
+  void LoadSettings(CRegKey& key);
+  void SaveSettings(CRegKey& key);
+  void PrefsChanged(CRegKey& key);
+
+protected:
+  DECLARE_MESSAGE_MAP()
+
+  afx_msg void OnPaint();
+  afx_msg void OnBackward();
+  afx_msg void OnForward();
+  afx_msg BOOL OnToolTipText(UINT, NMHDR*, LRESULT*);
+  afx_msg LRESULT OnIdleUpdateCmdUI(WPARAM, LPARAM);
+
+  void Create(CWnd* parent);
+  void SizeTab(CRect& client, CSize& fontSize, int& heading, int& h);
+
+  virtual CString GetToolTip(UINT_PTR id);
+
+private:
+  CButton m_navigate[2];
+};
