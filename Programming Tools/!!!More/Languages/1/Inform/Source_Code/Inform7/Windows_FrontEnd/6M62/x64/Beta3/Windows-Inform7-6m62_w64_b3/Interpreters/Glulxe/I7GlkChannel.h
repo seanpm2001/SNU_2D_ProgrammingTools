f6872629263907e3c344c3b0f1213f5fdd82a6ab
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-21 19:44:05 -0700

    Batch 2 - 1 sub-directory (34 files) (final batch)
    The 2nd (and final) batch of files (out of 2) for this directory, containing 34 files in various formats.

---
+++ Programming Tools/!!!More/Languages/1/Inform/Source_Code/Inform7/Windows_FrontEnd/6M62/x64/Beta3/Windows-Inform7-6m62_w64_b3/Interpreters/Glulxe/I7GlkChannel.h
@@ -0,0 +1,39 @@
+#pragma once
+
+extern "C" {
+#include "Glk.h"
+#include "gi_dispa.h"
+}
+
+#include <set>
+
+class I7GlkChannel
+{
+public:
+  I7GlkChannel(glui32 rock);
+  virtual ~I7GlkChannel();
+
+  int getId(void) { return m_id; }
+  glui32 getRock(void) { return m_rock; }
+  void setDispRock(const gidispatch_rock_t& rock) { m_dispRock = rock; }
+  gidispatch_rock_t& getDispRock(void) { return m_dispRock; }
+
+  glui32 play(glui32 snd, glui32 repeats, glui32 notify);
+  glui32 multiPlay(glui32 snd, glui32 repeats, glui32 notify, int* data);
+  void stop(bool check);
+  void setVolume(glui32 volume, glui32 duration, glui32 notify);
+  void pause(bool pause);
+  void getSoundNotify(event_t& event);
+  void getVolumeNotify(event_t& event);
+
+protected:
+  int m_id;
+  glui32 m_rock;
+  gidispatch_rock_t m_dispRock;
+
+  glui32 m_sound;
+  glui32 m_soundNotify;
+  glui32 m_volumeNotify;
+};
+
+extern std::set<I7GlkChannel*> glkChannels;
