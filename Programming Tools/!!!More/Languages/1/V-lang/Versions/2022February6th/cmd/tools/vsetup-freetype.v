2c3ac417b74079c06a6777944742b84f370930e6
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-02-09 15:22:57 -0800

    15 files
    15 files for this directory, in V format.

---
+++ Programming Tools/!!!More/Languages/1/V-lang/Versions/2022February6th/cmd/tools/vsetup-freetype.v
@@ -0,0 +1,28 @@
+module main
+
+import os
+import v.pref
+
+const freetype_repo_url = 'https://github.com/ubawurinna/freetype-windows-binaries'
+
+const freetype_folder = os.join_path('thirdparty', 'freetype')
+
+fn main() {
+	$if windows {
+		println('Setup freetype...')
+		vroot := os.dir(pref.vexe_path())
+		os.chdir(vroot) ?
+		if os.is_dir(freetype_folder) {
+			println('Thirdparty "freetype" is already installed.')
+		} else {
+			s := os.execute('git clone --depth=1 $freetype_repo_url $freetype_folder')
+			if s.exit_code != 0 {
+				panic(s.output)
+			}
+			println(s.output)
+			println('Thirdparty "freetype" installed successfully.')
+		}
+	} $else {
+		println('It is only for Windows to setup thirdparty "freetype".')
+	}
+}
